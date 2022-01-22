a6868adb71754afcbb9e0687a4a156556e685034
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-01-23 01:44:18 +0200

    basic-pdf-output

--- img-processor.py
+++ img-processor.py
@@ -1,12 +1,12 @@
 from PIL import Image, ImageOps, ImageDraw, ImageFont
 import os
 import sys
+import re
 
 # Append image and dependencies folder to path
 image_folder = '../images/'
 dependencies_folder = '../dependencies/'
 
-sys.path.append(image_folder)
 sys.path.append(dependencies_folder)
 
 # Import Name Dictionary
@@ -15,48 +15,63 @@ from settings import output_file_name
 
 # Returns image name given its number
 def get_image_name(number):
-    pass
+    return name_dictionary.get(number)
 
-# Get list of image paths
+def find_chars_until_dot(str):
+    find = re.compile(r"^[^.]*")
+    m = re.search(find, str).group(0)
+    return m
+
+# Get list of image names and a list of image paths
 def get_image_paths():
-    my_images = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
-    return my_images
+    image_name = [ f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    image_path = [image_folder + f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    return image_name, image_path
 
 # Turn list of images into a pdf
 def get_pdf(image_list):
     image_list[0].save("{}.pdf".format(output_file_name), save_all=True, append_images=images[1:], quality = 90)
 
 # Adds title and styling to an image
-def create_lookbook_image(number):
-    im=Image.open(path)
-    width, height = im.size
+def create_lookbook_image(image_path, name):
+    image = Image.open(image_path)
+    width, height = image.size
     f = ImageFont.truetype("PlayfairDisplay-VariableFont_wght.ttf", 150)
     txt=Image.new('L', (round(height * 0.6), 350) )
     d = ImageDraw.Draw(txt)
-    d.text( ( 0, 0), "ADONIS | TEST",  font=f, fill=255)
-    w=txt.rotate(90,  expand=1)
+    d.text( ( 0, 0), name,  font=f, fill=255)
+    w = txt.rotate(90,  expand=1)
     text_width, text_height = w.size
+    image.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (width - text_width, height - text_height - 200),  w)
 
-    im.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (width - text_width, height - text_height - 200),  w)
-    
-    return im
+    return image
 
 def main():
     
-    # Get image paths
+    # Get image names and paths
+    image_names, image_paths = get_image_paths()
+
+    # Initiate image list
+    ready_images_list = []
 
     # For i in images
-    # Check number at the start
+    for i in range(len(image_paths)):
 
-    # Get image name from number
-
-    # Print image name on image
-
-    # Add image in images list
-
+        # Check number at the start
+        try:
+            number = int(find_chars_until_dot(image_names[i]))
+        except:
+            print('Error: Image name doesn\'t start with a number --> ' + str(i))
+        # Get image name from number        
+        image_name = get_image_name(number)
+        # Print image name on image
+        ready_image = create_lookbook_image(image_paths[i], image_name)
+        print(type(ready_image))
+        # Add image in images list
+        ready_images_list.append(ready_image)
+        print(type(ready_images_list))
     # Make pdf from images
-
-    pass
+    ready_images_list[0].save("{}.pdf".format(output_file_name), save_all=True, append_images=ready_images_list[1:])
 
 if __name__ == '__main__':
     main()
