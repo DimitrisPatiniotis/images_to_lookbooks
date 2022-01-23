fa0ff735766b369ced9e8d6eec7af23a909acbd6
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-01-23 17:22:18 +0200

    basic-styling

--- img-processor.py
+++ img-processor.py
@@ -1,10 +1,8 @@
 from PIL import Image, ImageOps, ImageDraw, ImageFont
-import os
+from os import listdir, path, rename, remove
 import sys
 import re
 from PyPDF2 import PdfFileMerger
-
-
 
 # Append image and dependencies folder to path
 image_folder = '../images/'
@@ -13,8 +11,8 @@ dependencies_folder = '../dependencies/'
 sys.path.append(dependencies_folder)
 
 # Import Name Dictionary
-from name_dict import name_dictionary
-from settings import output_file_name, font_family, font_size, pdf_quality
+from name_dict import name_dictionary, code_name_dictionary
+from settings import output_file_name, font_family, font_size, pdf_quality, page_number, init_text_height, word_spacing, separator_size
 
 # Returns image name given its number
 def get_image_name(number):
@@ -27,8 +25,8 @@ def find_chars_until_dot(str):
 
 # Get list of image names and a list of image paths
 def get_image_paths():
-    image_name = [ f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
-    image_path = [image_folder + f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    image_name = [ f for f in listdir(image_folder) if path.isfile(path.join(image_folder, f))]
+    image_path = [image_folder + f for f in listdir(image_folder) if path.isfile(path.join(image_folder, f))]
     return image_name, image_path
 
 # Turn list of images into a pdf
@@ -39,30 +37,108 @@ def get_pdf(image_list):
 def text_transform(text):
     return text.replace('+', '|')
 
+
+def create_separator():
+    font = ImageFont.truetype(font_family, separator_size)
+    sep_width = font_size + 20
+    sep_height = font_size + 20
+    txt= Image.new('L', (sep_width, sep_height) )
+    d = ImageDraw.Draw(txt)
+    d.text(( 0, 0), '|', font=font, fill=255)
+    w = txt.rotate(90,  expand=1)
+
+    return w, sep_width, sep_height
+
+def print_page_name(name, nfont):
+    namelen=len(name)
+    name_width = round(namelen * font_size * 0.7)
+    name_height = font_size + 20
+    txt= Image.new('L', (name_width, name_height) )
+    d = ImageDraw.Draw(txt)
+    d.text(( 0, 0), text_transform(name.upper()), font=nfont, fill=255)
+    w = txt.rotate(90,  expand=1)
+
+    return w, name_width, name_height
+
+def print_cat_name(name, nfont):
+    print(name.lower())
+    cat = code_name_dictionary.get(name.lower())
+    cat_len = len(cat)
+    cat_width = round(cat_len * (font_size * 0.8))
+    cat_height = font_size + 20
+    txt= Image.new('L', (cat_width, cat_height) )
+    d = ImageDraw.Draw(txt)
+    d.text(( 0, 0), text_transform(cat.upper()), font=nfont, fill=255)
+    w = txt.rotate(0,  expand=1)
+
+    return w, cat_width, cat_height, cat
+
+def print_page_num(number, nfont):
+    page_number_len = len(str(number))
+    page_number_height = font_size + 20
+    number_text = Image.new('L',(page_number_len * 55, page_number_height))
+    n = ImageDraw.Draw(number_text)
+    n.text(( 0, 0), str(number),  font=nfont, fill=255)
+    l = number_text.rotate(0,  expand=1)
+
+    return l, page_number_height, page_number_len
+
 # Adds title and styling to an image
-def create_lookbook_image(image_path, name):
+def create_lookbook_image(image_path, name, pgnmbr):
 
     image = Image.open(image_path)
     width, height = image.size
-    f = ImageFont.truetype(font_family, font_size)
-    txt= Image.new('L', (round(height * 0.6), 350) )
-    d = ImageDraw.Draw(txt)
-    d.text(( 0, 0), text_transform(name.upper()),  font=f, fill=255)
-    w = txt.rotate(90,  expand=1)
-    text_width, text_height = w.size
-    image.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (width - text_width, height - text_height - 200),  w)
 
-    del d
-    del w
-    del txt
-    del width
-    del height
+    items = name.split(' + ')
+    num_items = len(items)
+    font = ImageFont.truetype(font_family, font_size)
 
+    total_height = init_text_height
+    padding = word_spacing
+
+    page_number = print_page_num(pgnmbr, font)
+    page_number_img = page_number[0]
+    page_number_height = page_number[1]
+    page_number_len = page_number[2]
+    image.paste( ImageOps.colorize(page_number_img, (0,0,0), (0,0,0)), (width - (300 - page_number_len * 10), height - (total_height + page_number_height)), page_number_img)
+
+    total_height += page_number_height + padding + 60
+
+    a_separator = create_separator()
+
+    for itemn in range(len(items)):
+        item = items[itemn]
+
+        page_main_name = print_page_name(item, font)
+        page_name = page_main_name[0]
+        page_name_height = page_main_name[1]
+        page_name_width = page_main_name[2]
+        image.paste( ImageOps.colorize(page_name, (0,0,0), (0,0,0)), (width - (190 + page_name_width), height - (total_height + page_name_height)), page_name)
+        total_height += (page_name_height)
+
+        cat_name, cat_height, cat_width, cat =  print_cat_name(item, font)
+        # image.paste( ImageOps.colorize(cat_name, (0,0,0), (0,0,0)), (width - (210 + cat_width), height - (total_height + cat_height)), cat_name)
+        if len(cat) == 3:
+            constw = 250
+        else:
+            constw = 210
+        if itemn == 0:
+            if pgnmbr == 1:
+                image.paste( ImageOps.colorize(cat_name, (0,0,0), (0,0,0)), (width - (constw + cat_width), height - (total_height)), cat_name)
+            else:
+                image.paste( ImageOps.colorize(cat_name, (0,0,0), (0,0,0)), (width - (constw + cat_width), height - (total_height + 30)), cat_name)
+        if itemn == 1:
+            image.paste( ImageOps.colorize(cat_name, (0,0,0), (0,0,0)), (width - (constw + cat_width), height - (total_height)), cat_name)
+        total_height += (cat_height + padding + 10)
+
+        if itemn != len(items) - 1:
+            separator_char, separator_height, separator_width = a_separator
+            image.paste( ImageOps.colorize(separator_char, (0,0,0), (0,0,0)), (width - (210 + cat_width - 8), height - (total_height + 20)), separator_char)
     return image
 
 # Get list of pdfs in directory
 def get_list_of_pdfs():
-    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".pdf")]
+    files = [f for f in listdir('.') if path.isfile(f) and f.endswith(".pdf")]
     return files
 
 def merge_pdfs(pdf_list):
@@ -82,9 +158,8 @@ def main():
     ready_images_list = []
     final_pdfs = 1
 
-    # images_num = len(image_names)
-    images_num = 32
-    # For i in images
+    images_num = 11
+
     for i in range(images_num):
         # Check number at the start
         try:
@@ -94,10 +169,11 @@ def main():
         # Get image name from number        
         image_name = get_image_name(number)
         # Print image name on image
-        ready_image = create_lookbook_image(image_paths[i], image_name)
+        
+        ready_image = create_lookbook_image(image_paths[i], image_name, i + 1)
         # Add image in images list
         ready_images_list.append(ready_image)
-        print(i)
+        # print(i)
 
         # Clear Memory
         del ready_image
@@ -108,19 +184,17 @@ def main():
             del ready_images_list
             ready_images_list = []
             final_pdfs += 1
-
-    # Concatinate all pdfs
+        print(i)
+    # Concatinate all pdfs, if more than 1 exist
     pdf_list = get_list_of_pdfs()
-    print(pdf_list)
     if len(pdf_list) > 1:
         merge_pdfs(pdf_list)
+        # Delete temp pdfs
+        for pdf in pdf_list:
+            if pdf != output_file_name + '.pdf':
+                remove(pdf)
     elif len(pdf_list) == 1:
-        os.rename(pdf_list[0], output_file_name)
-
-    # Delete temp pdfs
-    for pdf in pdf_list:
-        if pdf != output_file_name + '.pdf':
-            os.remove(pdf)
+        rename(pdf_list[0], output_file_name + '.pdf')
 
 if __name__ == '__main__':
     main()
