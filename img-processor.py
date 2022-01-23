21e21fea9602ae58ec7e203f311d34bc6350c830
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-01-23 03:05:19 +0200

    full-pdf-generation-with-basic-optimization

--- img-processor.py
+++ img-processor.py
@@ -2,6 +2,9 @@ from PIL import Image, ImageOps, ImageDraw, ImageFont
 import os
 import sys
 import re
+from PyPDF2 import PdfFileMerger
+
+
 
 # Append image and dependencies folder to path
 image_folder = '../images/'
@@ -11,7 +14,7 @@ sys.path.append(dependencies_folder)
 
 # Import Name Dictionary
 from name_dict import name_dictionary
-from settings import output_file_name
+from settings import output_file_name, font_family, font_size
 
 # Returns image name given its number
 def get_image_name(number):
@@ -32,19 +35,44 @@ def get_image_paths():
 def get_pdf(image_list):
     image_list[0].save("{}.pdf".format(output_file_name), save_all=True, append_images=images[1:], quality = 90)
 
+# Transform text from dictionary
+def text_transform(text):
+    return text.replace('+', '|')
+
 # Adds title and styling to an image
 def create_lookbook_image(image_path, name):
+
     image = Image.open(image_path)
     width, height = image.size
-    f = ImageFont.truetype("PlayfairDisplay-VariableFont_wght.ttf", 150)
-    txt=Image.new('L', (round(height * 0.6), 350) )
+    f = ImageFont.truetype(font_family, font_size)
+    txt= Image.new('L', (round(height * 0.6), 350) )
     d = ImageDraw.Draw(txt)
-    d.text( ( 0, 0), name,  font=f, fill=255)
+    d.text(( 0, 0), text_transform(name),  font=f, fill=255)
     w = txt.rotate(90,  expand=1)
     text_width, text_height = w.size
     image.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (width - text_width, height - text_height - 200),  w)
 
+    del d
+    del w
+    del txt
+    del width
+    del height
+
     return image
+
+# Get list of pdfs in directory
+def get_list_of_pdfs():
+    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".pdf")]
+    # files = filter(lambda f: f.endswith(('.pdf','.PDF')), files)
+    return files
+
+def merge_pdfs(pdf_list):
+    pdfs = pdf_list
+    merger = PdfFileMerger()
+    for pdf in pdfs:
+        merger.append(pdf)
+    merger.write(output_file_name + '.pdf')
+    merger.close()
 
 def main():
     
@@ -53,10 +81,12 @@ def main():
 
     # Initiate image list
     ready_images_list = []
+    final_pdfs = 1
 
+    images_num = len(image_names)
     # For i in images
-    for i in range(len(image_paths)):
-
+    # for i in range(len(image_paths)):
+    for i in range(images_num):
         # Check number at the start
         try:
             number = int(find_chars_until_dot(image_names[i]))
@@ -66,12 +96,36 @@ def main():
         image_name = get_image_name(number)
         # Print image name on image
         ready_image = create_lookbook_image(image_paths[i], image_name)
-        print(type(ready_image))
         # Add image in images list
         ready_images_list.append(ready_image)
-        print(type(ready_images_list))
+        print(i)
+
+        # Clear Memory
+        del ready_image
+
+        # Gets out of memory around i == 250 with 8gb ram and 4k images
+        if len(ready_images_list) == 10 or i == images_num - 1:
+            ready_images_list[0].save("{}.output.pdf".format(final_pdfs), save_all=True, append_images=ready_images_list[1:])
+            del ready_images_list
+            ready_images_list = []
+            final_pdfs += 1
+        
+
+
+    # Concatinate all pdfs
+    pdf_list = get_list_of_pdfs()
+    print(pdf_list)
+    if len(pdf_list) > 1:
+        merge_pdfs(pdf_list)
+    elif len(pdf_list) == 1:
+        os.rename(pdf_list[0], output_file_name)
+
+    for pdf in pdf_list:
+        if pdf != output_file_name + '.pdf':
+            os.remove(pdf)
+    
     # Make pdf from images
-    ready_images_list[0].save("{}.pdf".format(output_file_name), save_all=True, append_images=ready_images_list[1:])
+    # ready_images_list[0].save("{}.pdf".format(output_file_name), save_all=True, append_images=ready_images_list[1:])
 
 if __name__ == '__main__':
     main()
